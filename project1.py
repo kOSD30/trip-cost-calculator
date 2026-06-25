2697377035906eccdc75b08ea36a2203c7602c6e
Dominik <example@gmail.com>
2026-06-26 00:39:45 +0300

    Добавил сколько будет израсходавано топливо и сделал код более читабельным

--- project1.py
+++ project1.py
@@ -1,6 +1,11 @@
-print('Расход топлива калькулятор');
-km = float(input('Сколько километров: '));
-rashod = float(input('Какой расход топлива: '));
-cena = float(input('Цена топлива за литр: '));
-result = (km * rashod) /100 * cena;
-print(f'Стоимость поездки будет: {result:.2f} $');
+print('=== Расход топлива калькулятор ===');
+
+distance = float(input('Введите количество километров: '));
+rashod = float(input('Введите расход топлива(л/100): '));
+cena = float(input('Введите цену топлива за литр($): '));
+
+litry = (distance * rashod) /100;
+result = litry * cena;
+
+print(f"\nБудет израсходовано: {litry:.2f} л")
+print(f"Стоимость поездки: {result:.2f} €")
