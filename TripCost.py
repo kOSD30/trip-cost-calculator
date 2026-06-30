38dbc20275974b767febbda1fb2755f6df0afa29
Dominik <example@gmail.com>
2026-06-30 11:39:03 +0300

    add trip history

---
+++ TripCost.py
@@ -0,0 +1,40 @@
+print('=== Расход топлива калькулятор ===');
+
+distance = float(input('Введите количество километров: '));
+while distance < 0:
+    print('Ошибка!')
+    distance = float(input('Введите количество километров еще раз: '))
+
+rashod = float(input('Введите расход топлива(л/100): '));
+while rashod < 0:
+    print('Ошибка!')
+    rashod = float(input('Введите расход топлива еще раз(л/100): '))
+    
+toplivo = input('Какое у вас топливо бензин или дизель?')
+cena = float(input('Введите цену топлива за литр($): '));
+
+litry = (distance * rashod) /100;
+result = litry * cena;
+
+print('\n=== Результат ===')
+print(f'Дистанция поездки: {distance:.2f}')
+print(f'Расход топлива: {rashod:.2f} (л/100)')
+print(f'Вид топлива: {toplivo}')
+print(f'Цена топлива за литр: {cena:.2f} $')
+print(f"Будет израсходовано: {litry:.2f} л")
+print(f"Стоимость поездки: {result:.2f} €")
+
+text = f"""
+=== Результат ===
+Дистанция поездки: {distance:.2f}
+Расход топлива: {rashod:.2f}
+Вид топлива: {toplivo}
+Цена топлива: {cena:.2f} $
+Будет израсходовано: {litry:.2f} л
+Стоимость поездки: {result:.2f} €
+===========================
+"""
+
+f = open('historyTrip.txt', 'a', encoding='utf-8')
+f.write(text)
+f.close()
